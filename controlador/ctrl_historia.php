96f6e7f630c76747c30a6ee2baab97d8d6265601
Aaron Trujillo <93228184+STON3E187@users.noreply.github.com>
2025-09-06 00:53:54 -0400

    Feat: Controladores para las paginas de historia y MVV

---
+++ controlador/ctrl_historia.php
@@ -0,0 +1,10 @@
+<?php
+
+$head_data = [
+    "title" => "Historia - UNEXCA",
+
+    "meta" => [
+        "description" => "Conoce la historia de la UNEXCA y sus orígenes.",
+        "keywords" => "UNEXCA, historia, universidad, educación, fundación",
+    ],
+];
---
+++ controlador/ctrl_mision-vision-valores.php
@@ -0,0 +1,10 @@
+<?php
+
+$head_data = [
+    "title" => "Misión, Visión y Valores - UNEXCA",
+
+    "meta" => [
+        "description" => "Conoce la misión, visión y valores que guían a la UNEXCA como institución educativa.",
+        "keywords" => "UNEXCA, misión, visión, valores, universidad, educación",
+    ]
+];
