87a2b2c75b8d1745ecb107e8bff051b9518ee51d
Aaron Trujillo <93228184+STON3E187@users.noreply.github.com>
2025-09-06 00:41:37 -0400

    Refactor: agregado los link para el favicon

--- vista/plantilla/plantilla.php
+++ vista/plantilla/plantilla.php
@@ -7,6 +7,15 @@
     <link rel="stylesheet" href="<?= colocar_ruta_html('@estilos/paginas/general.css')?>">
     <link rel="stylesheet" href="<?= colocar_ruta_html('@estilos/componentes/footer.css')?>">
     <link rel="stylesheet" href="<?= colocar_ruta_html('@estilos/componentes/header.css')?>">
+
+    <!-- Iconos de las paginas -->
+    <link rel="shortcut icon" href="<?= colocar_ruta_html('@imagenes/iconos/favicon.ico') ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= colocar_ruta_html('@imagenes/iconos/favicon-16x16.png') ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= colocar_ruta_html('@imagenes/iconos/favicon-32x32.png') ?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= colocar_ruta_html('@imagenes/iconos/apple-touch-icon.png') ?>">
+    <link rel="icon" type="image/png" sizes="192x192" href="<?= colocar_ruta_html('@imagenes/iconos/android-chrome-192x192.png') ?>">
+    <link rel="icon" type="image/png" sizes="512x512" href="<?= colocar_ruta_html('@imagenes/iconos/android-chrome-512x512.png') ?>">
+
     <?php renderizar_head($head_data ?? []); ?>
 </head>
 <body>
