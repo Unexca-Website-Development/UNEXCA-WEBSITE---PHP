45b49a995a4e597a16b0e3fbc8758110810909d1
Aaron Trujillo <93228184+STON3E187@users.noreply.github.com>
2025-09-23 15:30:03 -0400

    Refactor: Simplificar la obtención de datos del footer y mejorar la renderización de enlaces

--- controlador/ctrl_plantilla.php
+++ controlador/ctrl_plantilla.php
@@ -1,10 +1,7 @@
 <?php
 require_once colocar_ruta_sistema('@servicios/plantilla/PlantillaServicio.php');
 
-//Esta linea para testear el footer
-require_once colocar_ruta_sistema('@modelo/plantilla/obtener_links_footer.php');
-
 $servicio = new PlantillaServicio();
 
 $data_header = $servicio->obtenerDatosMenu('Header');
-$data_footer = obtener_links_footer();
+$data_footer = $servicio->obtenerDatosMenu('Footer');
