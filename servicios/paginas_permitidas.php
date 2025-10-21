3c290732a3144e318b9892f43f99b22099f8d7c8
Aaron Trujillo <93228184+STON3E187@users.noreply.github.com>
2025-10-21 13:45:14 -0400

    Implementación del nuevo editor de noticias con bloques dinámicos
    - Creación de la clase EditorNoticias para gestionar la edición de noticias.
    - Implementación de bloques de contenido: texto, lista, imagen y cita.
    - Adición de funcionalidades para agregar, eliminar y mover bloques.
    - Integración de un sistema de control para la gestión de bloques en la interfaz.
    - Desarrollo de utilidades para la generación de IDs y SVGs.
    - Implementación de un servicio PHP para manejar la creación, actualización y eliminación de noticias y bloques en la base de datos.
    - Creación de la plantilla HTML para la interfaz del editor de noticias.

---
+++ servicios/paginas_permitidas.php
@@ -0,0 +1,15 @@
+<?php
+
+return $paginas_permitidas = [
+    'inicio' => 'inicio.php',
+    'historia' => 'historia.php',
+    'mision-vision-valores' => 'mision_vision_valores.php',
+    'autoridades' => 'autoridades.php',
+    'faqs' => 'faqs.php',
+    'servicios' => 'servicios.php',
+    'carrera' => 'carrera.php',
+    'contactos' => 'contactos.php',
+    'test' => 'test.php',
+    'noticias' => 'noticias.php',
+    'noticias-editor' => 'noticias_editor.php' //OJO con este porque maneja la DB
+];
