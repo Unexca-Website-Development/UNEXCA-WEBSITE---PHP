0bb8694f2a5b7dfe9b8b79ef91aba657dea08ede
Aaron Trujillo <93228184+STON3E187@users.noreply.github.com>
2025-10-22 16:03:27 -0400

    Refactor: Implementar el bloque de fechas en el editor de noticias y ajustar la inicialización de bloques en EditorNoticias

--- publico/scripts/editor/EditorNoticias.js
+++ publico/scripts/editor/EditorNoticias.js
@@ -1,20 +1,38 @@
-import { BloqueTexto, BloqueLista, BloqueImagen, BloqueCita } from './Bloques.js'
+import { BloqueTexto, BloqueLista, BloqueImagen, BloqueCita, BloqueFechas } from './Bloques.js'
 
 class EditorNoticias {
-	constructor(contenedor) {
-		this.contenedor = contenedor
-		this.bloques = []
+	constructor(contenedorCabecera, contenedorDinamico) {
+		this.contenedorCabecera = contenedorCabecera
+		this.contenedorDinamico = contenedorDinamico
+		this.bloquesCabecera = []
+		this.bloquesDinamicos = []
 		this.fechaCreacion = new Date()
 		this.fechaModificacion = null
 		this.fechaPublicacion = null
 	}
 
+	async inicializarCabecera() {
+		const bloqueFechas = new BloqueFechas('fechas', 'Información de la noticia', '/imagenes/iconos/icon_calendario.svg', this, false)
+		this.bloquesCabecera.push(bloqueFechas)
+		this.contenedorCabecera.appendChild(await bloqueFechas.renderizar())
+
+		const bloqueTitulo = new BloqueTexto('titulo', 'Título de la noticia', '/imagenes/iconos/icon_h1.svg', this, false)
+		this.bloquesCabecera.push(bloqueTitulo)
+		this.contenedorCabecera.appendChild(await bloqueTitulo.renderizar())
+
+		const bloqueDescripcion = new BloqueTexto('descripcion', 'Descripción de la noticia', '/imagenes/iconos/icon_descripcion.svg', this, false)
+		this.bloquesCabecera.push(bloqueDescripcion)
+		this.contenedorCabecera.appendChild(await bloqueDescripcion.renderizar())
+
+		const bloqueImagen = new BloqueImagen('imagen', 'Imagen principal', '/imagenes/iconos/icon_imagen.svg', this, false)
+		this.bloquesCabecera.push(bloqueImagen)
+		this.contenedorCabecera.appendChild(await bloqueImagen.renderizar())
+	}
+
 	async agregarBloque(tipo, texto, icono) {
 		let bloque
-		switch (tipo) {
+		switch(tipo){
 			case 'subtitulo':
-			case 'titulo':
-			case 'descripcion':
 			case 'parrafo':
 				bloque = new BloqueTexto(tipo, texto, icono, this)
 				break
@@ -30,48 +48,39 @@ class EditorNoticias {
 			default:
 				bloque = new BloqueTexto(tipo, texto, icono, this)
 		}
-		const elemento = await bloque.renderizar()
-		this.bloques.push(bloque)
-		this.contenedor.appendChild(elemento)
-		this.actualizarFechaModificacion()
+		this.bloquesDinamicos.push(bloque)
+		this.contenedorDinamico.appendChild(await bloque.renderizar())
 		return bloque
 	}
 
 	obtenerJSON() {
 		return {
-			fechaCreacion: this.fechaCreacion,
-			fechaModificacion: this.fechaModificacion,
-			fechaPublicacion: this.fechaPublicacion,
-			bloques: this.bloques.map(b => b.obtenerContenido())
+			cabecera: Object.fromEntries(this.bloquesCabecera.map(b => [b.tipo, b.obtenerContenido()])),
+			bloques: this.bloquesDinamicos.map(b => b.obtenerContenido())
 		}
-	}
-
-	guardarNoticia() {
-		this.fechaModificacion = new Date()
-		return this.obtenerJSON()
-	}
-
-	publicarNoticia() {
-		this.fechaPublicacion = new Date()
-		this.actualizarFechaModificacion()
-		return this.obtenerJSON()
-	}
-
-	actualizarFechaModificacion() {
-		this.fechaModificacion = new Date()
 	}
 
 	async cargarNoticia(data) {
-		this.contenedor.innerHTML = ''
-		this.bloques = []
-		for (const b of data.bloques) {
-			const bloque = await this.agregarBloque(b.tipo, b.texto, b.icono)
-			if (b.contenido) bloque.asignarContenido(b.contenido)
-		}
-		this.fechaCreacion = new Date(data.fechaCreacion)
-		this.fechaModificacion = new Date(data.fechaModificacion)
-		this.fechaPublicacion = data.fechaPublicacion ? new Date(data.fechaPublicacion) : null
-	}
-};
+		this.contenedorCabecera.innerHTML = ''
+		this.contenedorDinamico.innerHTML = ''
+		this.bloquesCabecera = []
+		this.bloquesDinamicos = []
 
-export {EditorNoticias};
+		await this.inicializarCabecera()
+
+		if(data.cabecera){
+			for(const bloque of this.bloquesCabecera){
+				if(data.cabecera[bloque.tipo]) bloque.asignarContenido(data.cabecera[bloque.tipo])
+			}
+		}
+
+		if(data.bloques){
+			for(const b of data.bloques){
+				const bloque = await this.agregarBloque(b.tipo, b.texto, b.icono)
+				if(b.contenido) bloque.asignarContenido(b.contenido)
+			}
+		}
+	}
+}
+
+export { EditorNoticias }
