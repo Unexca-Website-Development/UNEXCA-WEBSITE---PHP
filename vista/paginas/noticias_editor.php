e883c9f24628d71e5c03edd90dd50f438fe50ff5
Aaron Trujillo <93228184+STON3E187@users.noreply.github.com>
2025-11-10 22:20:21 -0400

    Refactor: reorganizar el editor de noticias, implementar nuevas secciones y mejorar la gestión de bloques

--- vista/paginas/noticias_editor.php
+++ vista/paginas/noticias_editor.php
@@ -1,3 +1,4 @@
-<div id="editor-principal"></div>
+<div class="principal" id="editor-principal">
+</div>
 
-<script type="module" src="<?= colocar_ruta_html('@scripts/EditorNoticias/main.js')?>"></script>
+<script type="module" src="<?= colocar_ruta_html('@scripts/EditorNoticias/ui/EditorUi.js')?>"></script>
