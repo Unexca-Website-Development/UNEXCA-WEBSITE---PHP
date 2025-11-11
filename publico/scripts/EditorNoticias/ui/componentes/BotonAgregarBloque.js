e8a2a40f7393bb45eb241f8d04bbc784c3b60246
Aaron Trujillo <93228184+STON3E187@users.noreply.github.com>
2025-11-10 22:20:21 -0400

    Refactor: reorganizar el editor de noticias, implementar nuevas secciones y mejorar la gestión de bloques

---
+++ publico/scripts/EditorNoticias/ui/componentes/BotonAgregarBloque.js
@@ -0,0 +1,25 @@
+import IconoSVG from './IconoSVG.js'
+
+export default class BotonAgregarBloque {
+	constructor(rutaIcono, texto, tipo) {
+		this.boton = document.createElement('button')
+		this.boton.type = 'button'
+		this.boton.className = 'agregar-bloque__opcion'
+		this.tipo = tipo
+		this.rutaIcono = rutaIcono
+		this.texto = texto
+	}
+
+	async renderizar() {
+		const icono = new IconoSVG(this.rutaIcono)
+		await icono.cargar()
+		this.boton.appendChild(icono.renderizar())
+
+		const span = document.createElement('span')
+		span.className = 'agregar-bloque__texto'
+		span.textContent = this.texto
+		this.boton.appendChild(span)
+
+		return this.boton
+	}
+}
