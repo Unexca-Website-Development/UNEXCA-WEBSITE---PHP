a55343a27b66775a233eb367def83dc95782166a
Aaron Trujillo <93228184+STON3E187@users.noreply.github.com>
2025-11-11 00:24:36 -0400

    Refactor: mejorar la creación de botones en el editor de noticias utilizando una función utilitaria

--- publico/scripts/EditorNoticias/ui/componentes/TextareaBloque.js
+++ publico/scripts/EditorNoticias/ui/componentes/TextareaBloque.js
@@ -1,7 +1,6 @@
 export default class TextareaBloque {
-	constructor(id, key, placeholder = '', requerido = false) {
+	constructor(id, placeholder = '', requerido = false) {
 		this.id = id
-		this.key = key
 		this.placeholder = placeholder
 		this.requerido = requerido
 		this.elemento = null
@@ -13,7 +12,6 @@ export default class TextareaBloque {
 		textarea.id = this.id
 		textarea.placeholder = this.placeholder
 		if (this.requerido) textarea.required = true
-		textarea.setAttribute('data-key', this.key)
 
 		this.elemento = textarea
 		return textarea
