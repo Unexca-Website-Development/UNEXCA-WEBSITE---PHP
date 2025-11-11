16e1b74e5c78bb3b62ea281898ff25a61bb55452
Aaron Trujillo <93228184+STON3E187@users.noreply.github.com>
2025-11-11 00:24:36 -0400

    Refactor: mejorar la creación de botones en el editor de noticias utilizando una función utilitaria

--- publico/scripts/EditorNoticias/ui/componentes/BotonAgregarBloque.js
+++ publico/scripts/EditorNoticias/ui/componentes/BotonAgregarBloque.js
@@ -1,24 +1,19 @@
-import { crearIcono } from '../utilidadesUI.js'
+import { crearBoton } from '../utilidadesUI.js'
 
 export default class BotonAgregarBloque {
 	constructor(rutaIcono, texto, tipo) {
-		this.boton = document.createElement('button')
-		this.boton.type = 'button'
-		this.boton.className = 'agregar-bloque__opcion'
 		this.tipo = tipo
 		this.rutaIcono = rutaIcono
 		this.texto = texto
 	}
 
 	async renderizar() {
-		const icono = await crearIcono(this.rutaIcono)
-		this.boton.appendChild(icono.renderizar())
-
-		const span = document.createElement('span')
-		span.className = 'agregar-bloque__texto'
-		span.textContent = this.texto
-		this.boton.appendChild(span)
-
-		return this.boton
+		return await crearBoton({
+			rutaIcono: this.rutaIcono,
+			texto: this.texto,
+			clase: 'agregar-bloque__opcion',
+			tipo: 'button',
+			claseSpan: 'agregar-bloque__texto'
+		})
 	}
 }
--- publico/scripts/EditorNoticias/ui/utilidadesUI.js
+++ publico/scripts/EditorNoticias/ui/utilidadesUI.js
@@ -1,5 +1,8 @@
 import IconoSVG from "./componentes/IconoSVG.js"
 import LabelBloque from "./componentes/LabelBloque.js"
+import TextareaBloque from "./componentes/TextareaBloque.js"
+import InputBloque from "./componentes/InputBloque.js"
+import Boton from "./componentes/Boton.js"
 
 export async function crearIcono(ruta, clase) {
 	const icono = new IconoSVG(ruta, clase)
@@ -10,4 +13,19 @@ export async function crearIcono(ruta, clase) {
 export async function crearLabelBloque(id, texto, rutaIcono) {
 	const label = new LabelBloque(id, texto, rutaIcono)
 	return await label.renderizar()
+}
+
+export function crearTextareaBloque(id, placeholder = '', requerido = false) {
+	const textarea = new TextareaBloque(id, placeholder, requerido)
+	return textarea.renderizar()
+}
+
+export function crearInputBloque(id, tipo = '', requerido = false, aceptar = ''){
+	const input = new InputBloque(id, tipo, requerido, aceptar)
+	return input.renderizar()
+}
+
+export async function crearBoton({ rutaIcono = '', texto = '', clase = '', tipo = 'button', mostrarTexto = true, claseSpan = 'boton__texto' } = {}) {
+	const boton = new Boton({ rutaIcono, texto, clase, tipo, mostrarTexto, claseSpan })
+	return await boton.renderizar()
 }
