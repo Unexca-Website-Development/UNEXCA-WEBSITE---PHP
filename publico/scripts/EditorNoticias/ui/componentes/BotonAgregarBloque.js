71d585d774a736b5858b61f65cd31b9c8044d43c
Aaron Trujillo <93228184+STON3E187@users.noreply.github.com>
2025-11-13 01:57:12 -0400

    Refactor: implementar patrón singleton en EditorControlador y optimizar la creación de bloques en BotonAgregarBloque

--- publico/scripts/EditorNoticias/ui/componentes/BotonAgregarBloque.js
+++ publico/scripts/EditorNoticias/ui/componentes/BotonAgregarBloque.js
@@ -1,19 +1,27 @@
 import { crearBoton } from '../utilidadesUI.js'
+import EditorControlador from '../../controladores/EditorControlador.js'
 
 export default class BotonAgregarBloque {
 	constructor(rutaIcono, texto, tipo) {
 		this.tipo = tipo
 		this.rutaIcono = rutaIcono
 		this.texto = texto
+		this.controlador = new EditorControlador()
 	}
 
 	async renderizar() {
-		return await crearBoton({
+		const boton = await crearBoton({
 			rutaIcono: this.rutaIcono,
 			texto: this.texto,
 			clase: 'agregar-bloque__opcion',
 			tipo: 'button',
 			claseSpan: 'agregar-bloque__texto'
 		})
+
+		boton.addEventListener('click', () => {
+			this.controlador.agregarBloque(this.tipo)
+		})
+
+		return boton
 	}
 }
