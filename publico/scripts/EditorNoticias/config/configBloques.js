6d1e97de5a5d64015c44eb89f2e36a514fbea291
Aaron Trujillo <93228184+STON3E187@users.noreply.github.com>
2025-11-14 00:05:11 -0400

    Refactor: simplificar la gestión de contenido en bloques y optimizar la configuración de campos en el editor de noticias

--- publico/scripts/EditorNoticias/config/configBloques.js
+++ publico/scripts/EditorNoticias/config/configBloques.js
@@ -2,141 +2,81 @@ const CONFIG_BLOQUES = {
 	titulo: {
 		tipo: 'titulo',
 		texto: 'Título',
-		campos: {
-			texto: { valorInicial: '', requerido: true }
-		},
+		campos: { texto: '' },
 		ui: {
 			icono: '/imagenes/iconos/icon_h1.svg',
 			inputs: [
-				{
-					tipo: 'textarea',
-					requerido: true,
-					placeholder: 'Escribe el título aquí...',
-					className: 'editor-noticia__campo-texto'
-				}
+				{ tipo: 'textarea', key: 'texto', requerido: true, placeholder: 'Escribe el título aquí...', className: 'editor-noticia__campo-texto' }
 			]
 		}
 	},
 	subtitulo: {
 		tipo: 'subtitulo',
 		texto: 'Subtítulo',
-		campos: {
-			texto: { valorInicial: '', requerido: true }
-		},
+		campos: { texto: '' },
 		ui: {
 			icono: '/imagenes/iconos/icon_h2.svg',
 			inputs: [
-				{
-					tipo: 'textarea',
-					requerido: true,
-					placeholder: 'Escribe el Subtítulo aquí...',
-					className: 'editor-noticia__campo-texto'
-				}
+				{ tipo: 'textarea', key: 'texto', requerido: true, placeholder: 'Escribe el Subtítulo aquí...', className: 'editor-noticia__campo-texto' }
 			]
 		}
 	},
 	fechas: {
 		tipo: 'fechas',
 		texto: 'Información de la noticia',
-		campos: {
-			creacion: { valorInicial: '', requerido: true },
-			modificacion: { valorInicial: '', requerido: true },
-			publicacion: { valorInicial: '', requerido: true }
-		},
+		campos: { creacion: '', modificacion: '', publicacion: '' },
 		ui: {
 			icono: '/imagenes/iconos/icon_calendario.svg',
 			inputs: [
-				{
-					tipo: 'date',
-					requerido: true,
-					className: 'editor-noticia__campo-fecha'
-				}
+				{ tipo: 'date', key: 'creacion', requerido: true, className: 'editor-noticia__campo-fecha' },
+				{ tipo: 'date', key: 'modificacion', requerido: true, className: 'editor-noticia__campo-fecha' },
+				{ tipo: 'date', key: 'publicacion', requerido: true, className: 'editor-noticia__campo-fecha' }
 			]
 		}
 	},
 	parrafo: {
 		tipo: 'parrafo',
 		texto: 'Párrafo',
-		campos: {
-			texto: { valorInicial: '', requerido: true }
-		},
+		campos: { texto: '' },
 		ui: {
 			icono: '/imagenes/iconos/icon_parrafo.svg',
 			inputs: [
-				{
-					tipo: 'textarea',
-					requerido: true,
-					placeholder: 'Escribe el párrafo aquí...',
-					className: 'editor-noticia__campo-texto'
-				}
+				{ tipo: 'textarea', key: 'texto', requerido: true, placeholder: 'Escribe el párrafo aquí...', className: 'editor-noticia__campo-texto' }
 			]
 		}
 	},
 	cita: {
 		tipo: 'cita',
 		texto: 'Cita',
-		campos: {
-			texto: { valorInicial: '', requerido: true },
-			autor: { valorInicial: '', requerido: true }
-		},
+		campos: { texto: '', autor: '' },
 		ui: {
-			icono: '/imagenes/iconos/icon_cita.svg',	
+			icono: '/imagenes/iconos/icon_cita.svg',
 			inputs: [
-				{
-					tipo: 'textarea',
-					requerido: true,
-					placeholder: 'Escribe la cita aquí...',
-					className: 'editor-noticia__campo-texto'
-				},
-				{
-					tipo: 'text',
-					placeholder: 'Autor de la cita',
-					requerido: true,
-					className: 'editor-noticia__campo-texto'
-				}
+				{ tipo: 'textarea', key: 'texto', requerido: true, placeholder: 'Escribe la cita aquí...', className: 'editor-noticia__campo-texto' },
+				{ tipo: 'textarea', key: 'autor', requerido: true, placeholder: 'Autor de la cita', className: 'editor-noticia__campo-texto' }
 			]
 		}
 	},
 	lista: {
 		tipo: 'lista',
 		texto: 'Lista',
-		campos: {
-			items: { valorInicial: [], requerido: true }
-		},
+		campos: { items: [] },
 		ui: {
 			icono: '/imagenes/iconos/icon_lista.svg',
 			inputs: [
-				{
-					tipo: 'textarea',
-					requerido: true,
-					placeholder: 'Agrega elementos de la lista...',
-					className: 'editor-noticia__campo-texto'
-				}
+				{ tipo: 'textarea', key: 'items', requerido: true, placeholder: 'Agrega elementos de la lista...', className: 'editor-noticia__campo-texto' }
 			]
 		}
 	},
 	imagen: {
 		tipo: 'imagenes',
-		texto: 'imagenes',
-		campos: {
-			archivo: { valorInicial: null, requerido: true },
-			descripcion: { valorInicial: '', requerido: true }
-		},
+		texto: 'Imagenes',
+		campos: { archivo: '', descripcion: '' },
 		ui: {
 			icono: '/imagenes/iconos/icon_imagen.svg',
 			inputs: [
-				{
-					tipo: 'file',
-					aceptar: '.png,.jpg,.jpeg,.webp',
-					requerido: true,
-					className: 'editor-noticia__campo-archivo'
-				},
-				{
-					tipo: 'textarea',
-					requerido: true,
-					placeholder: 'Descripción de la imagenes...',
-					className: 'editor-noticia__campo-texto'
-				}
+				{ tipo: 'file', key: 'archivo', aceptar: '.png,.jpg,.jpeg,.webp', requerido: true, className: 'editor-noticia__campo-archivo' },
+				{ tipo: 'textarea', key: 'descripcion', requerido: true, placeholder: 'Descripción de la imagen...', className: 'editor-noticia__campo-texto' }
 			]
 		}
 	}
