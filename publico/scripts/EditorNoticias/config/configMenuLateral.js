43beb930c3fc048c575454f61550e33af67a12ce
Aaron Trujillo <93228184+STON3E187@users.noreply.github.com>
2025-11-15 16:35:26 -0400

    Refactor: implementar nuevoDocumento y guardarNoticia en el controlador de editor, y configurar el menú lateral

---
+++ publico/scripts/EditorNoticias/config/configMenuLateral.js
@@ -0,0 +1,43 @@
+export const CONFIG_MENU_LATERAL = [
+	{
+		id: 'btn-menu-abrir',
+		rutaIcono: '/imagenes/iconos/icon_menu_open.svg',
+		clase: 'menu-editor__boton'
+	},
+	{
+		id: 'btn-nueva-noticia',
+		rutaIcono: '/imagenes/iconos/icon_nueva.svg',
+		texto: 'Nueva noticia',
+		clase: 'menu-editor__boton',
+		claseSpan: 'menu-editor__texto'
+	},
+	{
+		id: 'btn-buscar-noticia',
+		rutaIcono: '/imagenes/iconos/icon_buscar.svg',
+		texto: 'Buscar noticia',
+		clase: 'menu-editor__boton',
+		claseSpan: 'menu-editor__texto'
+	},
+	{
+		id: 'btn-guardar-noticia',
+		rutaIcono: '/imagenes/iconos/icon_guardar.svg',
+		texto: 'Guardar noticia',
+		clase: 'menu-editor__boton',
+		claseSpan: 'menu-editor__texto'
+	},
+	{
+		id: 'btn-publicar-noticia',
+		rutaIcono: '/imagenes/iconos/icon_publicar.svg',
+		texto: 'Publicar noticia',
+		clase: 'menu-editor__boton',
+		claseSpan: 'menu-editor__texto'
+	}
+]
+
+export const CONFIG_MENU_RECIENTES = {
+	id: 'btn-noticias-recientes',
+	rutaIcono: '/imagenes/iconos/flecha.svg',
+	texto: 'Noticias recientes',
+	clase: 'menu-editor__boton menu-editor__boton--recientes',
+	claseSpan: 'menu-editor__texto'
+}
