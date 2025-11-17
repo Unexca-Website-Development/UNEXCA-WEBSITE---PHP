c16da0be910592005c639613d209fb30b68cb69a
Aaron Trujillo <93228184+STON3E187@users.noreply.github.com>
2025-11-17 01:12:53 -0400

    Fix: Añadir contenido y estructura a la página de Preguntas Frecuentes (FAQs) borrado por error

--- vista/paginas/faqs.php
+++ vista/paginas/faqs.php
@@ -17,3 +17,12 @@
  * - desplegables.js: Maneja la interacción de abrir/cerrar los desplegables.
  */
 ?>
+<main class="main__general">
+    <section class="seccion">
+        <h2 class="titulos">Preguntas Frecuentes</h2>
+        <div class="desplegables__contenedor">
+            <?php renderizar_lista_desplegable($data_faqs); ?>
+        </div>
+    </section>
+</main>
+<script src="<?= colocar_ruta_html('@scripts/desplegables.js')?>"></script>
