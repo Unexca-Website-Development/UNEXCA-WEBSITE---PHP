10c2cc2607e1665f142d67f95eaba1c2dde8b024
Aaron Trujillo <93228184+STON3E187@users.noreply.github.com>
2026-02-24 17:33:29 -0400

    Feat: Nueva plantilla vacia

---
+++ vista/plantilla/vacia.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ES">
+        <?php head($head_data ?? []); ?>
+    <body>
+        <div class="main__contenedor" id="main">
+            <?php include $vista; ?>
+        </div>
+    </body>
+</html>
